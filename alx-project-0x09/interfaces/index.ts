b5e88b3594e99783f2b1ea624876bd7501e6e7dc
Abdi begna <abditadesse89@gmail.com>
2025-12-09 21:21:42 +0300

    updating

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -1,3 +1,19 @@
+// import { ReactNode } from "react";
+
+// export interface ReactComponentProps {
+//   children: ReactNode;
+// }
+
+// export interface GeneratedImageProps {
+//   imageUrl: string;
+//   prompt: string;
+//   width?: string;
+//   height?: string;
+//   action: (imagePath: string) => void;
+// }
+
+// export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
+
 import { ReactNode } from "react";
 
 export interface ReactComponentProps {
@@ -11,5 +27,9 @@ export interface GeneratedImageProps {
   height?: string;
   action: (imagePath: string) => void;
 }
+
+export type RequestProps = {
+  prompt: string;
+};
 
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
